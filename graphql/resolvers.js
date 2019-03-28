6acfccf00615316f1f6981aabf716354b5336c16
hoxi777 <jtho1015@gmail.com>
2019-03-28 15:50:41 +0900

    Add Graphql Server

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        name: () =>"Teho"
+    }
+}
+
+export default resolvers;
---
+++ index.js
@@ -0,0 +1,10 @@
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
+
+const server = new GraphQLServer({
+    //환경설정
+    typeDefs: "graphql/schema.graphql",
+    resolvers
+});
+
+server.start(() => console.log("Graphql Server Running"));
